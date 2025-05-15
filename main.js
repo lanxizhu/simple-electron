1c0016c5df80c2e52b6cefcade41e5e3257e0f61
lanxiz <mr.longzhentian@gmail.com>
2025-05-15 10:00:34 +0800

    feat: dev with vite

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile("index.html");
+  // mainWindow.loadFile("index.html");
+  mainWindow.loadURL("http://localhost:5173");
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
